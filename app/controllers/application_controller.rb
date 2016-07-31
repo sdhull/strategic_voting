238d31ae05a1290db884b1a71f1226f5b520455e
Steve Hull <p.witty@gmail.com>
2016-07-30 18:39:49 -0700

    Update allowed parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:state, :desired_candidate, :name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:state, :desired_candidate, :name])
   end
 end
